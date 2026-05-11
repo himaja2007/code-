e179625591bd662e3789037c763aed3c4ea4bfac
himaja2007 <arikatlahimajareddy07@gmail.com>
2026-05-11 08:57:35 -0700

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,9 @@
+arr=[64,36,74,12,85,7]
+for i in range(len(arr)):
+    min_index=i
+    for j in range(i+1,len(arr)):
+        if(arr[j]<arr[min_index]):
+            min_index=j
+    arr[i],arr[min_index]=arr[min_index],arr[i]
+print("sorted:")
+print(arr)            
